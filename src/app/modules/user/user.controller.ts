80c9ae4ba9214afbf29cb0820bee8f4009a0eb34
rafi10hasan <rafihasan0075@gmail.com>
2026-05-16 09:25:01 +0600

    code updated

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -21,56 +21,7 @@ const createAccountIntoDb = asyncHandler(async (req: Request, res: Response) => 
   });
 });
 
-// update user location
-const updateUserLocationIntoDb = asyncHandler(async (req: Request, res: Response) => {
-  const result = await userService.updateUserLocation(req.user, req.body);
-  // console.log(result);
-  sendResponse(res, {
-    statusCode: StatusCodes.OK,
-    success: true,
-    message: 'User location has been updated successfully',
-    data: result,
-  });
-});
-
-const updateUserProfileImageIntoDb = asyncHandler(async (req: Request, res: Response) => {
-  console.log(req.files)
-  const result = await userService.updateUserProfileImage(req.user, req.files as TProfileImage);
-  // console.log(result);
-  sendResponse(res, {
-    statusCode: StatusCodes.OK,
-    success: true,
-    message: 'User profile image has been updated successfully',
-    data: result,
-  });
-})
-
-const getUserShortInfoIntoDb = asyncHandler(async (req: Request, res: Response) => {
-  const result = await userService.getUserShortInfo(req.user);
-  sendResponse(res, {
-    statusCode: StatusCodes.OK,
-    success: true,
-    message: 'User short info has been retrieved successfully',
-    data: result,
-  });
-});
-
-const switchUserRoleIntoDb = asyncHandler(async (req: Request, res: Response) => {
-  const result = await userService.switchUserRole(req.user);
-  // console.log(result);
-  const isProfileCompleteRequired = 'status' in result && result.status === 'INCOMPLETE_PROFILE';
-  sendResponse(res, {
-    statusCode: StatusCodes.OK,
-    success: true,
-    message: isProfileCompleteRequired ? `User role cannot be switched: ${result.status}` : 'User role has been switched successfully',
-    data: result,
-  });
-});
-
 export const userController = {
   createAccountIntoDb,
-  updateUserLocationIntoDb,
-  switchUserRoleIntoDb,
-  updateUserProfileImageIntoDb,
-  getUserShortInfoIntoDb
+ 
 };
