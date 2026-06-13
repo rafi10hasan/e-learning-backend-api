02229faaa715d61aed1733daad7e619dfcd6767c
rafi10hasan <rafihasan0075@gmail.com>
2026-06-13 09:14:44 +0600

    get me api created

--- src/app/modules/dashboard/index.ts
+++ src/app/modules/dashboard/index.ts
@@ -2,6 +2,9 @@ import { Router } from "express";
 import dashboardQuestionRouter from "./question/question.route";
 import userManagementRouter from "./user-management/user.management.route";
 import userOverviewRouter from "./overview/overview.route";
+import { adminController } from "./admin/admin.controller";
+import authMiddleware from "../../middlewares/auth.middleware";
+import { USER_ROLE } from "../user/user.constant";
 
 
 const adminRouter = Router();
@@ -10,6 +13,7 @@ const adminRouter = Router();
 adminRouter.use('/questions', dashboardQuestionRouter);
 adminRouter.use('/users', userManagementRouter);
 adminRouter.use('/overview', userOverviewRouter);
+adminRouter.use('/get-me', authMiddleware(USER_ROLE.SUPER_ADMIN), adminController.getMeIntoDb);
 
 
 export default adminRouter;
